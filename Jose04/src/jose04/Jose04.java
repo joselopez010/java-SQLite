afe07b5348225ac477b3cc20eed9251159b2c03b
alumno <informatica@ipleones.cl>
2016-09-08 11:24:00 -0300

    creacion de la clase principal

---
+++ Jose04/src/jose04/Jose04.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jose04;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Jose04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        // TODO code application logic here
+    }
+    
+}
